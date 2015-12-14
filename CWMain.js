6685b000a3a6573cf48e482c1026e896c8e169b0
Mike McGuire <mcguire@mullenhigh.com>
2015-12-14 10:08:16 -0700

    add tabs

---
+++ CWMain.js
@@ -0,0 +1,44 @@
+var ngSkeleton = angular.module('ngSkeleton', ['ngRoute']);
+
+ngSkeleton.config(function($routeProvider) 
+    {
+    $routeProvider
+        .when('/', 
+              {
+              templateUrl : 'pages/home.html',
+              controller  : 'mainController'
+			  })
+        .when('/about', 
+              {
+              templateUrl : 'pages/about.html',
+              controller  : 'aboutController'
+			  })
+        .when('/contact', 
+              {
+              templateUrl : 'pages/contact.html',
+              controller  : 'contactController'
+              });
+	});
+
+ngSkeleton.controller('mainController', function($scope) 
+    {
+    $scope.message = 'Everyone come and see how good I look!';
+	});
+
+ngSkeleton.controller('aboutController', function($scope) 
+    {
+    $scope.message = 'Look! I am an about page.';
+	});
+
+ngSkeleton.controller('contactController', function($scope) 
+    {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+	});
+
+$(document).on('click','.navbar-collapse.in',function(e)
+    {
+    if( $(e.target).is('a') ) 
+        {
+        $(this).collapse('hide');
+        }
+    });
